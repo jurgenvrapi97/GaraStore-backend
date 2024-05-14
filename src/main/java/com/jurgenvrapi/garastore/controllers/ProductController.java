83729459fab7d4c12bd72919a508b279525734d4
jurgenvrapi97 <jurgenvrapi@gmail.com>
2024-05-14 13:37:34 +0200

    upload image fixed

--- src/main/java/com/jurgenvrapi/garastore/controllers/ProductController.java
+++ src/main/java/com/jurgenvrapi/garastore/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.jurgenvrapi.garastore.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jurgenvrapi.garastore.entities.Product;
 import com.jurgenvrapi.garastore.services.ImageUploadService;
 import com.jurgenvrapi.garastore.services.ProductService;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -31,10 +33,16 @@ public class ProductController {
         return productService.getProductById(id).orElseThrow(() -> new RuntimeException("Product not found"));
     }
 
-@PostMapping
-public Product createProduct(@RequestPart("product") Product product, @RequestPart("image") MultipartFile image) {
-    return productService.createProduct(product, image);
-}
+    @PostMapping
+    public Product createProduct(@RequestPart("product") String productString, @RequestPart("image") MultipartFile image) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Product product = mapper.readValue(productString, Product.class);
+            return productService.createProduct(product, image);
+        } catch (IOException e) {
+            throw new RuntimeException("Error parsing product data", e);
+        }
+    }
 
     @PutMapping("/{id}")
     public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
